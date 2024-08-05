11971cd3bb2067aef3202fb84e7361b0c32cdbd2
Shubham <shubham@velocius.ai>
2024-08-05 14:53:14 +0530

    dummy commit

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def extract_fields_from_text(text: str):
 
 @app.post("/hello")
 async def hello_user(name: str):
-    return f"Hello {name}, How's you!"
+    return f"Hello {name}"
 
 @app.post("/upload")
 async def upload_pdf(file: UploadFile = File(...)):
